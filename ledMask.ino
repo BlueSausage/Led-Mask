d3abf25affef10359adc9e78189ae54255c816c9
BlueSausage <36741399+BlueSausage@users.noreply.github.com>
2021-01-07 21:17:59 +0100

    Update ledMask.ino
    Added bluetooth functionality

--- ledMask.ino
+++ ledMask.ino
@@ -1,10 +1,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 #define MATRIXPIN 13
 #define MICPIN 34
 
+BluetoothSerial SerialBT;
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, MATRIXPIN,
                             NEO_MATRIX_BOTTOM + NEO_MATRIX_LEFT +
                             NEO_MATRIX_ROWS   + NEO_MATRIX_PROGRESSIVE,
@@ -98,6 +104,8 @@ const uint16_t timeMouthClosed = 1000;
 uint16_t color[4] = {};
 uint16_t brightness = 100;
 
+String message = "";
+
 void drawImage(int image[][8]) {
   for (int y = 0; y < 8; y++) {
     for (int x = 0; x < 8; x++) {
@@ -114,7 +122,7 @@ void setMouth(int mouth[][8]) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   matrix.begin();
   matrix.setBrightness(brightness);
 
@@ -125,9 +133,47 @@ void setup() {
 
   matrix.fillScreen(color[0]);
   matrix.show();
+  
+  SerialBT.begin("Led-Mask");
 }
 
 void loop() {
+  /* Ideas:
+   *  - Add sound visualization
+   */
+  if (SerialBT.available()) {
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n') {
+      message += String(incomingChar);
+    } else {
+      message = "";
+    }
+  }
+  delay(20);
+  
+  if (message == "sad") {
+    drawImage(sad);
+    delay(2000);
+  }
+  if (message == "+") {
+    if(brightness++ > 255) {
+      brightness = 0;
+    }
+    Serial.println(brightness);
+    matrix.setBrightness(brightness);
+    matrix.show();
+    delay(10);
+  }
+  if (message == "-") {
+    if(brightness-- < 0) {
+      brightness = 255;
+    }
+    Serial.println(brightness);
+    matrix.setBrightness(brightness);
+    matrix.show();
+    delay(10);
+  }
+  
   maxVol = 0;
 
   for (int i = 0; i < samples; i++) {
